729143065b343139cd616ea452f60844ad97e580
Michal Faciszewski <michal.faciszewski@premotec.ch>
2024-01-10 22:36:42 +0100

    Review fixes

--- areq/areq.py
+++ areq/areq.py
@@ -22,27 +22,24 @@ class Areq:
 
     def __init__(self, username: str, host: Hosts = "ares.cyfronet.pl", pkey_path: str = None, password: str = None) -> None:
         self._host = host
-        self.username = username
-        self.ssh = self.establish_ssh_session(password, pkey_path)
+        self._username = username
+        self._ssh = self.establish_ssh_session(password, pkey_path)
         
     def establish_ssh_session(
         self, 
         password: str, 
         pkey_path: str
-    ) -> paramiko.SSHClient | None:
+    ) -> paramiko.SSHClient:
         ssh = paramiko.SSHClient()
-        try:     
-            ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
-            if pkey_path:
-                pkey = paramiko.RSAKey(filename=pkey_path)
-                ssh.connect(self._host, username=self.username, pkey=pkey)
-            elif password:
-                ssh.connect(self._host, username=self.username, password=password)
-            else:
-                raise ValueError("Either 'password' or 'private_key_path' must be provided.")
-            print("Established ssh session successfully.")
-        except Exception as e:
-            print(f"Unable to establish ssh sesion. Error: {e}")
+        ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
+        if pkey_path:
+            pkey = paramiko.RSAKey(filename=pkey_path)
+            ssh.connect(self._host, username=self._username, pkey=pkey)
+        elif password:
+            ssh.connect(self._host, username=self._username, password=password)
+        else:
+            raise ValueError("Either 'password' or 'private_key_path' must be provided.")
+        print("Established ssh session successfully.")
         return ssh
             
     def _request(self, method: str, path: str, *args, **kwargs) -> requests.Response:
@@ -84,14 +81,12 @@ class Areq:
         self,
         local_file_path: str,
         remote_file_path: str
-    ) -> bool:
+    ) -> None:
         try:
-            sftp = self.ssh.open_sftp()
+            sftp = self._ssh.open_sftp()
             sftp.put(local_file_path, remote_file_path)
             sftp.close()
             print(f"Uploaded file successfully to path: {remote_file_path}")
-            return True
         except Exception as e:
             print(f"Unable to upload the file. Error: {e}")
-        return False
  
