256b03415dc3a17bf5cb3dc9181840bfbf8c49be
robertkbk <33332309+robertkbk@users.noreply.github.com>
2024-01-11 22:56:03 +0100

    Added other endpoints

--- areq/areq.py
+++ areq/areq.py
@@ -4,31 +4,64 @@ from typing import Literal, TypeAlias
 
 import requests
 import paramiko
+import urllib.parse
 
 from options import Options, to_sbatch_options
 from response import Error, SubmitResponse
+
 Hosts: TypeAlias = Literal["ares.cyfronet.pl"]
 
 
-def _build_script(lines: Sequence[str], options: Options) -> str:
+def _build_script(lines: Sequence[str], options: Options, shebang: str | None) -> str:
     if lines[0].startswith("#!"):
-        return "\n".join([lines[0], *to_sbatch_options(options), *lines[1:]])
+        shebang = lines[0]
 
-    return "\n".join([*to_sbatch_options(options), *lines])
+    else:
+        if shebang is None:
+            raise ValueError(
+                "Interpreter not specified! "
+                "Either specify shebang in script or select interpreter"
+            )
+
+        shebang = shebang
+
+    return "\n".join([shebang, *to_sbatch_options(options), *lines[1:]])
 
 
 class Areq:
     _BASE_URL = "https://submit.plgrid.pl/api/"
 
-    def __init__(self, username: str, host: Hosts = "ares.cyfronet.pl", pkey_path: str = None, password: str = None) -> None:
+    def __init__(
+        self,
+        username: str,
+        proxy_path: str | bytes | os.PathLike | None = None,
+        host: Hosts = "ares.cyfronet.pl",
+        interpreter: str | None = "/bin/sh",
+        pkey_path: str | None = None,
+        password: str | None = None,
+    ) -> None:
         self._host = host
+
+        if interpreter is None:
+            self._shebang = None
+        else:
+            self._shebang = (
+                interpreter if interpreter.startswith("#!") else f"#!{interpreter}"
+            )
+
+        self._proxy = None
+
+        if proxy_path:
+            with open(proxy_path, "rb") as proxy_file:
+                proxy = proxy_file.read()
+                proxy = base64.encodebytes(proxy)
+                self._proxy = proxy.replace(b"\n", b"")
+
         self._username = username
         self._ssh = self.establish_ssh_session(password, pkey_path)
-        
+
     def establish_ssh_session(
-        self, 
-        password: str, 
-        pkey_path: str
+        self, password: str, pkey_path: str
     ) -> paramiko.SSHClient | Error:
         ssh = paramiko.SSHClient()
         ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
@@ -38,15 +71,23 @@ class Areq:
         elif password:
             ssh.connect(self._host, username=self._username, password=password)
         else:
-            raise ValueError("Either 'password' or 'private_key_path' must be provided.")
+            raise ValueError(
+                "Either 'password' or 'private_key_path' must be provided."
+            )
         print("Established ssh session successfully.")
         return ssh
-            
-    def _request(self, method: str, path: str, *args, **kwargs) -> requests.Response:
-        # TODO: Authentication
-        return requests.request(method, self._BASE_URL + path, *args, **kwargs)
 
-    def create(
+    def _request(
+        self, method: str, path: str, *, headers: dict[str, str] | None = None, **kwargs
+    ) -> requests.Response:
+        return requests.request(
+            method,
+            self._BASE_URL + path,
+            headers={**(headers or {}), "PROXY": self._proxy},
+            **kwargs,
+        )
+
+    def submit(
         self,
         script: str,
         options: Options,
@@ -57,7 +98,7 @@ class Areq:
 
         body: dict[str, object] = {
             "host": self._host,
-            "script": _build_script(lines, options),
+            "script": _build_script(lines, options, self._shebang),
         }
 
         if working_directory is not None:
@@ -66,58 +107,72 @@ class Areq:
         response = self._request("POST", "jobs", json=body)
         return response.json()
 
-    def status(
+    def statuses(
         self,
-        job_ids: list[str],
+        job_id: str | list[str],
         tag: str | None = None,
         format: str | None = None,
     ) -> list[SubmitResponse] | Error:
         response = self._request(
-            "GET", "jobs", params={"job_id": job_ids, "tag": tag, "format": format}
+            "GET", "jobs", params={"job_id": job_id, "tag": tag, "format": format}
         )
         return response.json()
 
-    def upload_file(
-        self,
-        local_file_path: str,
-        remote_file_path: str
-    ) -> None:
+    def upload_file(self, local_file_path: str, remote_file_path: str) -> None:
         sftp = self._ssh.open_sftp()
         sftp.put(local_file_path, remote_file_path)
         sftp.close()
         print(f"Uploaded file successfully to path: {remote_file_path}")
-            
-    def download_file(
-        self,
-        remote_file_path: str,
-        local_file_path: str
-    ) -> None:
+
+    def download_file(self, remote_file_path: str, local_file_path: str) -> None:
         sftp = self._ssh.open_sftp()
         sftp.get(remote_file_path, local_file_path)
         sftp.close()
         print(f"Downloaded file successfully to path: {remote_file_path}")
- 
-    def create_and_download_proxy(
-        self,
-        proxy_passphrase: str,
-        local_file_path: str
-    ) -> None:
-        _, stdout, stderr = self._ssh.exec_command(f'echo {proxy_passphrase} | grid-proxy-init -out ~/proxy -pwstdin', get_pty=True)
-        
-        stderr_string = stderr.read().decode('ascii')
-        stdout_string = stdout.read().decode('ascii')
-        print(stdout_string, stderr_string)
-        if len(stderr_string) > 0:
-            raise Error(f'Got remote error : {stderr_string}')
-        
-        _, stdout, stderr = self._ssh.exec_command('cat ~/proxy| base64 | tr -d \'\\n\' > proxy2', get_pty=True)
-        
-        stderr_string = stderr.read().decode('ascii')
-        stdout_string = stdout.read().decode('ascii')
-        print(stdout_string, stderr_string)
-        if len(stderr_string) > 0:
-            raise Error(f'Got remote error : {stderr_string}')
-        
-        self.download_file(f'/net/people/plgrid/{self._username}/proxy2', local_file_path)
 
-        
+    def create_and_download_proxy(
+        self, proxy_passphrase: str, local_file_path: str
+    ) -> None:
+        _, stdout, stderr = self._ssh.exec_command(
+            f"echo {proxy_passphrase} | grid-proxy-init -out ~/proxy -pwstdin",
+            get_pty=True,
+        )
+
+        stderr_string = stderr.read().decode("ascii")
+        stdout_string = stdout.read().decode("ascii")
+        print(stdout_string, stderr_string)
+        if len(stderr_string) > 0:
+            raise Error(f"Got remote error : {stderr_string}")
+
+        _, stdout, stderr = self._ssh.exec_command(
+            "cat ~/proxy| base64 | tr -d '\\n' > proxy2", get_pty=True
+        )
+
+        stderr_string = stderr.read().decode("ascii")
+        stdout_string = stdout.read().decode("ascii")
+        print(stdout_string, stderr_string)
+        if len(stderr_string) > 0:
+            raise Error(f"Got remote error : {stderr_string}")
+
+        self.download_file(
+            f"/net/people/plgrid/{self._username}/proxy2", local_file_path
+        )
+
+    def status(self, job_id: str) -> SubmitResponse | Error:
+        id_quoted = urllib.parse.quote(job_id)
+        response = self._request("GET", f"jobs/{id_quoted}")
+        return response.json()
+
+    def delete(self, job_id: str) -> Error | None:
+        id_quoted = urllib.parse.quote(job_id)
+        response = self._request("DELETE", f"jobs/{id_quoted}")
+
+        if not response.ok:
+            return response.json()
+
+    def abort(self, job_id: str) -> Error | None:
+        id_quoted = urllib.parse.quote(job_id)
+        response = self._request("PUT", f"jobs/{id_quoted}", json={"action": "abort"})
+
+        if not response.ok:
+            return response.json()
--- areq/response.py
+++ areq/response.py
@@ -1,18 +1,19 @@
-from typing import TypedDict
+from typing import Literal, TypedDict
 
 
 class ResponseBase(TypedDict):
-    status: str
     tag: str | None
 
 
 class SubmitResponse(ResponseBase):
+    status: Literal["QUEUED", "FINISHED"]
     job_id: str
     stdout_path: str
     stderr_path: str
 
 
 class Error(ResponseBase):
+    status: Literal["ERROR"]
     exit_code: int
     standard_output: str
     error_output: str
